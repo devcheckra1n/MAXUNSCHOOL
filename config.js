48751fb74c6c0174c7e48a557f33aba6b1e279b7
_checkra1n <97061690+devcheckra1n@users.noreply.github.com>
2023-10-14 19:47:39 -0400

    money for fun
    wry

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://maxim.bakaofsuss.workers.dev",
+  VITE_TMDB_READ_API_KEY: "a1db964e25023e4d66f2ecfefb042ffe"
+};
