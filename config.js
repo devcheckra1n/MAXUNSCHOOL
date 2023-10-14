7a08bed4664d31a932365000ebb8b2757c1b8323
_checkra1n <97061690+devcheckra1n@users.noreply.github.com>
2023-10-14 19:51:00 -0400

    oopsies mistake

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://maxim.bakaofsuss.workers.dev",
-  VITE_TMDB_READ_API_KEY: "a1db964e25023e4d66f2ecfefb042ffe"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMWRiOTY0ZTI1MDIzZTRkNjZmMmVjZmVmYjA0MmZmZSIsInN1YiI6IjY1MmIyNmEyZjI4ODM4MDI5ZjMyNDkyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HdJcqOkwE1qHVcaQpfvCgRniQTqTYuqOadFPlC4zer8"
 };
